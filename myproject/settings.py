3f4639e4516b3a9c95a2623b5ab810c6ef91576e
rihyun1126 <rihyun1126@gmail.com>
2019-07-31 19:19:16 +0900

    최종 중앙과제

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '*lc6vf1v9kh8d86!tn=$bg5*#0yetg
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
