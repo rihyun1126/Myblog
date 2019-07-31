67ca0e5ccc8cc3a83754addbe1a1e2db4e923c6d
rihyun1126 <rihyun1126@gmail.com>
2019-07-31 19:13:33 +0900

    중앙과제

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,11 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*lc6vf1v9kh8d86!tn=$bg5*#0yetgw2o=6pl@bg@o7#di_nc8'
+#SECRET_KEY = '*lc6vf1v9kh8d86!tn=$bg5*#0yetgw2o=6pl@bg@o7#di_nc8'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '*lc6vf1v9kh8d86!tn=$bg5*#0yetgw2o=6pl@bg@o7#di_nc8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+#DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 ALLOWED_HOSTS = []
 
 
